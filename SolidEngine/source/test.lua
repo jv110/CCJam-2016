85d9ab3f1a61de9ac387c44fbd21ce1a1450fdc3
João Victor <bebezos@hotmail.com>
2016-07-03 01:14:19 -0300

    Update test
    A deformed Link falls onto a weird ground while a part of the Zelda theme plays! Click to jump.

--- SolidEngine/source/test.lua
+++ SolidEngine/source/test.lua
@@ -1,23 +1,42 @@
 @include gameobject/sprite.lua
-@include graphics/camera.lua
+@include graphics/camera2d.lua
+@include graphics/mesh.lua
+@include physics/aabb.lua
+@include physics/body.lua
 @include level/level.lua
+@include sound/sound.lua
 @include game.lua
 
-os.loadAPI("blittle")
+local cam = camera2d.new()
+local lvl = level.new()
+local sp = sprite.new(texture.load([[
+@include assets/textures/link
+]]))
 
-local cam = camera.new()
-local lev = level.new()
-local sp = sprite.new(texture.new(0, 0, {colors.blue, colors.yellow, colors.red, colors.magenta}))
-sp.pos = vec4.new(-0.5, -0.5, 1, 1)
-lev:addObject(sp)
+sp.scale = vec3.new(0.0625, 0.0625, 1) 
 
-local ot = os.clock()
+local b = body.new()
+
+sp:addComponent(aabb.new():matchParent())
+sp:addComponent(b)
+
+local sp2 = sprite.new(texture.new(1, 1, {colors.blue, colors.yellow, colors.red, colors.magenta}))
+
+sp2.pos.y = -0.5
+sp2.scale.x = 16
+
+sp2:addComponent(aabb.new():matchParent())
+
+lvl:addObject(sp)
+lvl:addObject(sp2)
+
+sound.playSound(sound.load("Í}ÿÿÿ}ÿ}}}}ÿnÿ}"))
 
 game = {
   inputmanagers = {
     {
       handleEvent = function(self, event, p1, p2, p3, p4, p5)
-        if event == "mouse_click" then print "lol" end
+        if event == "mouse_click" then b.velocity.y = 0.25 end
       end
     }
   },
@@ -25,19 +44,20 @@ game = {
   fps = 20,
   
   init = function(self)
-    
+    local termWidth, termHeight = term.getSize()
+    cam:viewport(1, 1, termWidth * 2, termHeight * 3, true)
+    b.velocity.x = 0.125
   end,
   
   update = function(self)
-    
+    lvl:update()
+    term.setCursorPos(1, 1)
+    print(time.deltaTime / 20)
   end,
   
   render = function(self)
-    cam:render(lev)
-    local t = os.clock()
-    term.setCursorPos(1, 1)
-    print("Took", (t - ot) / 0.05, "ticks")
-    ot = t
+    cam:clear(colors.black)
+    cam:render(lvl)
   end
 }
 
